5b1e000ddb0ba63752f5d6783882c9f53c8b1501
Pătru Vlad Daniel <patru.vlad_daniel@yahoo.com>
2020-10-05 16:42:50 +0300

    Added Task entity and repository

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -8,7 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class TasksController extends AbstractController
+class TaskController extends AbstractController
 {
     private function getTasks(){
         return [
---
+++ src/Entity/Task.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TaskRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=TaskRepository::class)
+ */
+class Task
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $text;
+
+    /**
+     * @ORM\Column(type="time")
+     */
+    private $hours;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getText(): ?string
+    {
+        return $this->text;
+    }
+
+    public function setText(string $text): self
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    public function getHours(): ?\DateTimeInterface
+    {
+        return $this->hours;
+    }
+
+    public function setHours(\DateTimeInterface $hours): self
+    {
+        $this->hours = $hours;
+
+        return $this;
+    }
+}
