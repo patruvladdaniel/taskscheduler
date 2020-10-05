4cdf4619dd49391936e2e90c56856897e157802a
Pătru Vlad Daniel <patru.vlad_daniel@yahoo.com>
2020-10-05 17:17:12 +0300

    Updated migration to insert dummy data

--- migrations/Version20201005134355.php
+++ migrations/Version20201005134355.php
@@ -21,6 +21,9 @@ final class Version20201005134355 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE task (id INT AUTO_INCREMENT NOT NULL, text VARCHAR(255) NOT NULL, hours TIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('INSERT INTO task(text, hours) VALUES ("Lorem ipsum dolor sit amet",  "08:00:00" )');
+        $this->addSql('INSERT INTO task(text, hours) VALUES ("Ut enim ad minim veniam", "03:30:00" )');
+        $this->addSql('INSERT INTO task(text, hours) VALUES ("Duis aute irure dolor in reprehenderit in voluptate velit", "13:00:00" )');
     }
 
     public function down(Schema $schema) : void
